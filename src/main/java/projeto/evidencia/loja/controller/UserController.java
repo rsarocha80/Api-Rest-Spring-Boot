896d9ab7216691310a546352ac10fd72efe36ed0
Rodrigo Rocha <rodrigosantosrocha80@gmail.com>
2022-07-26 10:38:36 -0300

    primeiro endpoint, findAll

--- src/main/java/projeto/evidencia/loja/controller/UserController.java
+++ src/main/java/projeto/evidencia/loja/controller/UserController.java
@@ -1,10 +1,27 @@
 package projeto.evidencia.loja.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import projeto.evidencia.loja.dto.UserDTO;
+import projeto.evidencia.loja.servicies.UserServices;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+	@Autowired
+	private UserServices services;
+
+	@GetMapping
+	public ResponseEntity<List<UserDTO>> findAll() {
+		return ResponseEntity.ok(services.findAll());
+
+	}
 
 }
--- src/main/java/projeto/evidencia/loja/servicies/UserServices.java
+++ src/main/java/projeto/evidencia/loja/servicies/UserServices.java
@@ -1,8 +1,42 @@
 package projeto.evidencia.loja.servicies;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import projeto.evidencia.loja.dto.UserDTO;
+import projeto.evidencia.loja.entities.User;
+import projeto.evidencia.loja.repositories.UserRepository;
 
 @Service
 public class UserServices {
 
+	private static Logger logger = LoggerFactory.getLogger(UserServices.class);
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<UserDTO> findAll() {
+		logger.info("find all entities");
+		List<User> user = repository.findAll();
+		return user.stream().map(this::converterEntityForDTO).collect(Collectors.toList());
+
+	}
+
+	// conversores
+
+	public UserDTO converterEntityForDTO(User user) {
+
+		UserDTO dto = new UserDTO();
+		dto.setName(user.getName());
+		dto.setEmail(user.getEmail());
+		dto.setPassword(user.getPassword());
+		dto.setPhone(user.getPhone());
+
+		return dto;
+	}
 }
